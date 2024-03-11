bfc1aff1e01bba465cb303e99f95e66c0b790a1b
Arthur Xavier <garok102@gmail.com>
2024-03-11 16:26:05 -0300

    fix: namespace declarations

--- src/EloquentTracy.php
+++ src/EloquentTracy.php
@@ -1,27 +1,43 @@
 <?php
 
+namespace Arthurx\EloquentTracy;
+
 class EloquentTracy implements \Tracy\IBarPanel
 {
+    public function __construct(
+        private string $title = 'DB'
+    ) { }
+
     public function getPanel()
     {
-        $svg = file_get_contents(realpath(__DIR__ . PHP_EOL . 'assets' . PHP_EOL . 'database.sv'));
+        $queryLog = \Illuminate\Database\Capsule\Manager::connection()->getRawQueryLog();
+        $totalQueries = count($queryLog);
+        $totalTime = array_column($queryLog, 'time');
+        $totalTime = array_reduce($totalTime, function($carry, $current) {
+            return $carry + (float) $current;
+        }, 0.0);
+        $log = '';
 
-        return "<span title=\"Explaining tooltip\">
-            {$svg}
-            <span class=\"tracy-label\">Title</span>
-        </span>";
+        foreach ($queryLog as $v) {
+            $log .= "<p><b>QUERY:&nbsp;</b>{$v['raw_query']}<br><b>TIME:&nbsp;</b>{$v['time']}</p>";
+        }
+
+        return "<h1>Database</h1>
+        <div class=\"tracy-inner\">
+            <div class=\"tracy-inner-container\">
+                <b>{$totalQueries} queries performed</b> in <b>{$totalTime}ms</b>
+                <pre>{$log}</pre>
+            </div>
+        </div>";
     }
 
     public function getTab()
     {
-        $queryLog = \Illuminate\Database\Capsule\Manager::connection()->getQueryLog();
-        $queryLog = var_export($queryLog, true);
+        $svg = file_get_contents(__DIR__ . '/assets/database.svg');
 
-        return "<h1>Title</h1>
-        <div class=\"tracy-inner\">
-            <div class=\"tracy-inner-container\">
-                {$queryLog}
-            </div>
-        </div>";
+        return "<span title=\"Database queries\">
+            {$svg}
+            <span class=\"tracy-label\">{$this->title}</span>
+        </span>";
     }
 }
